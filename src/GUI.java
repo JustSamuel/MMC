4c446e8cd2f8564a8eaba55f3da57e9c71d4cef5
Samuel <samuel.oosterholt20001004@gmail.com>
2019-12-15 19:32:01 +0100

    Initial commit to create P3 interface

---
+++ src/GUI.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+/**
+ * GUI for the Modulo Multiplication Circle
+ */
+public class GUI extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("GUI");
+    }
+
+    public void settings(){
+
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+
+}
